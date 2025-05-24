986cbb192fc984c00dcbed97f7ed482e03e59291
Gabor <gabor.nemeth.de@gmail.com>
2025-05-24 08:11:24 +0200

    Description mező hozzáadva, SQL és Card osztály változtatása

--- CardGame/Model/Card.cs
+++ CardGame/Model/Card.cs
@@ -3,6 +3,7 @@
     public class Card
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public int Hp { get; set; }
         public int Mp { get; set; }
         public int MoveSpeed { get; set; }
