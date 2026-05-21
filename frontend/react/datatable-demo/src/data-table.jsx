4bb60d1f7166b46e44c2e29b1d5fc56abcfe640e
Gabor <gabor.nemeth.de@gmail.com>
2026-05-21 07:03:41 +0200

    mod: data-table

---
+++ frontend/react/datatable-demo/src/data-table.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+export default function DataTable({url}) {
+    const [userData, setUserData] = useState({})
+
+    useEffect(
+        () => {
+
+        },
+        [])
+
+    return (
+        <div className="data-table">
+
+        </div>
+    )
+}
